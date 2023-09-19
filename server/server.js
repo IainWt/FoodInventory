82a4eaf14d1f123098718d84f1037c2c7ee7ce28
IainWt <weetmaniain@gmail.com>
2023-09-19 20:40:01 +0100

    Add .env and mongoose connection messages

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
+require("dotenv").config()
+
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const Food = require("./Food")
 
 const app = express()
 app.use(cors)
 app.use(express.json)
 
-mongoose.connect("mongodb://localhost/")
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on("error", error => console.error(error))
+db.once("open", () => console.log("Connected to database"))
+
+async function addFood(foodData) {
+  const newFood = Food.create({foodData})
+  console.log(newFood)
+}
+
+async function openFood(foodData) {
+  foodData.open = true
+
+}
 
 app.get("/api", (req, res) => {
   res.json({"users": ["user1", "user2", "user3"]})
